160abbb2e1fb073901de99d5d290ac25f1400b27
lauranjansen <lauran.jansen@gmail.com>
2015-01-22 12:10:15 -0500

    Add initial framework for Mars Rover assignment

---
+++ mars_rover.rb
@@ -0,0 +1,19 @@
+class Rover
+	def initialize(xpos, ypos, direction)
+		@x = xpos
+		@y = ypos
+		@dir = direction
+	end
+
+	def read_instruction
+		
+	end
+
+	def move
+
+	end
+
+	def turn
+		
+	end
+end
