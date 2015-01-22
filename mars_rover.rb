69fbde452f683743cf062d5500d305812d932dac
lauranjansen <lauran.jansen@gmail.com>
2015-01-22 12:25:30 -0500

    Add turn and move methods

--- mars_rover.rb
+++ mars_rover.rb
@@ -2,18 +2,41 @@ class Rover
 	def initialize(xpos, ypos, direction)
 		@x = xpos
 		@y = ypos
-		@dir = direction
+		@direction = direction
 	end
 
-	def read_instruction
-		
+	def read_instruction(instructions)
+		instructions.each do |instruction|
+
+		end
 	end
 
 	def move
-
+		case direction
+		when "N"
+			@y += 1
+		when "E"
+			@x += 1
+		when "S"
+			@y -= 1
+		when "W"
+			@x -= 1
+		end
 	end
 
-	def turn
-		
+	def turn(rotation)
+		if (@direction == "N" && rotation == "L") || (@direction == "S" && rotation == "R")
+			@direction = "W"
+			puts "Turning west!"
+		elsif (@direction == "E" && rotation == "L") || (@direction == "W" && rotation == "R")
+			@direction = "N"
+			puts "Turning north!"
+		elsif (@direction == "S" && rotation == "L") || (@direction == "N" && rotation == "R")
+			@direction = "E"
+			puts "Turning east!"
+		else
+			@direction = "S"
+			puts "Turning south!"
+		end
 	end
 end
