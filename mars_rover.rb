b126bac0c7e1833cdf0f438716cc7aacc3bf3a1a
lauranjansen <lauran.jansen@gmail.com>
2015-01-22 13:41:54 -0500

    Add input and output

--- mars_rover.rb
+++ mars_rover.rb
@@ -5,14 +5,18 @@ class Rover
 		@direction = direction
 	end
 
-	def read_instruction(instructions)
-		instructions.each do |instruction|
-
+	def read_instructions(instructions)
+		instructions.each_char do |instruction|
+			if instruction == "M"
+				move
+			elsif instruction == "L" || instruction == "R"
+				turn(instruction)
+			end
 		end
 	end
 
 	def move
-		case direction
+		case @direction
 		when "N"
 			@y += 1
 		when "E"
@@ -27,16 +31,38 @@ class Rover
 	def turn(rotation)
 		if (@direction == "N" && rotation == "L") || (@direction == "S" && rotation == "R")
 			@direction = "W"
-			puts "Turning west!"
+			# puts "Turning west!"
 		elsif (@direction == "E" && rotation == "L") || (@direction == "W" && rotation == "R")
 			@direction = "N"
-			puts "Turning north!"
+			# puts "Turning north!"
 		elsif (@direction == "S" && rotation == "L") || (@direction == "N" && rotation == "R")
 			@direction = "E"
-			puts "Turning east!"
+			# puts "Turning east!"
 		else
 			@direction = "S"
-			puts "Turning south!"
+			# puts "Turning south!"
 		end
 	end
+
+	def to_s
+		"#{@x} #{@y} #{@direction}"
+	end
 end
+
+print "Please enter the size of the plateau: "
+plateau_size = gets.chomp
+
+print "Enter the initial position and direction of the first rover: "
+init1 = gets.chomp
+rover1 = Rover.new(init1[0].to_i,init1[2].to_i,init1[4].upcase) 
+print "Enter the instructions for the first rover: "
+rover1.read_instructions(gets.chomp.upcase)
+
+print "Enter the initial position and direction of the second rover: "
+init2 = gets.chomp
+rover2 = Rover.new(init2[0].to_i,init2[2].to_i,init2[4].upcase) 
+print "Enter the instructions for the second rover: "
+rover2.read_instructions(gets.chomp.upcase)
+
+puts rover1
+puts rover2
